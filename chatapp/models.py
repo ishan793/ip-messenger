6c6b1cca8af06e1550dc652b0d696a210c5d78e8
unknown <rajshah@iitgn.ac.in>
2013-11-21 23:27:56 +0530

    push

--- chatapp/models.py
+++ chatapp/models.py
@@ -6,6 +6,7 @@ from django.db.models.fields.related import ManyToManyField, ForeignKey
 
 # This model is reponsible for signup, Roll number should be the primary key.
 class SignUp(models.Model):
+        """The model describes the sql table representing user"""
 	user_name = models.CharField("Username", max_length = 100,unique=True, primary_key=True)
 	email = models.EmailField("Email", max_length = 100)	
 	roll_number = models.IntegerField("EnrollemntId", max_length = 10 ,unique=True)
@@ -15,20 +16,21 @@ class SignUp(models.Model):
 	def __unicode__ (self):
         	return self.user_name
 class OnlinePresence(models.Model):
+        """The model describes the sql table for online users"""
 	user_name = models.CharField("Username", max_length = 100,unique=True)
 	presence = models.IntegerField("EnrollemntId", max_length = 10)
 	ping_time=models.BigIntegerField("Time")
 	def __unicode__ (self):
 		return self.user_name
 
-# Class representing the table for a message
+# 
 class message(models.Model):
+        """Class representing the table for a message"""
 	message_content = models.CharField('Content', max_length = 100)
 	message_status = models.IntegerField('Status', max_length = 1)	
 	message_send=ForeignKey(SignUp,related_name='Sender+')
         message_recieve=ForeignKey(SignUp,related_name='Reciever+')
-
-        # giving some problems, time stamp of a different place and issues in django admin page
+        
         message_send_time=models.DateTimeField()
         message_recieve_time=models.DateTimeField()
     
